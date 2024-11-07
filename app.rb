4ea4155c0a458f244201ecc5e500d5b016f9bab5
David Cannan <104325852+Cdaprod@users.noreply.github.com>
2024-11-07 12:14:42 -0500

    Add logging for requests and uploads in app.rb

--- app.rb
+++ app.rb
@@ -3,9 +3,18 @@ require 'sinatra'
 require 'json'
 require 'fileutils'
 
+# Enable logging
+set :logging, true
+
 # Store uploads in the storage directory
 STORAGE_DIR = '/app/storage'
 FileUtils.mkdir_p(STORAGE_DIR)
+
+# Debug logging for all requests
+before do
+  puts "Request: #{request.request_method} #{request.path}"
+  puts "Params: #{params.inspect}"
+end
 
 # Root route
 get '/' do
@@ -14,6 +23,10 @@ end
 
 # Upload endpoint - this is what asciinema client POSTs to
 post '/api/asciicasts' do
+  puts "Received POST to /api/asciicasts"
+  puts "Content-Type: #{request.content_type}"
+  puts "Params: #{params.inspect}"
+  
   # Get the uploaded file from the request
   cast_file = params['asciicast']
   
